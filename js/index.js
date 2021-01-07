b8bebaeabb137f9985f12320c06f0c45a8d2a2f2
yvsdarahas <yvsdarahas@gmail.com>
2021-01-07 22:57:21 +0530

    Added properties Add, Remove and Delete tasks

--- js/index.js
+++ js/index.js
@@ -20,17 +20,16 @@ function AddTaskToDisplay() {
         let listElement = document.createElement("li");
         listElement.setAttribute("class", task.className);
         let titleOfTask = document.createElement("h1");
+        titleOfTask.setAttribute("id", "task__title"+task.className);
+        titleOfTask.setAttribute("class", task.className);
         let descriptionOfTask = document.createElement("p");
+        descriptionOfTask.setAttribute("id", "task__desc"+task.className);
+        descriptionOfTask.setAttribute("class", task.className);
 
-        //Create buttons ADD and DELETE
-        let buttonDone = document.createElement("BUTTON")
-        let buttonText = document.createTextNode("Done");
-        buttonDone.setAttribute("class", "buttonDone__"+task.className);
-        buttonDone.appendChild(buttonText);
-        
+        //Create button DELETE        
         let buttonDelete = document.createElement("BUTTON");
         buttonText2 = document.createTextNode("Delete");
-        buttonDelete.setAttribute("class", "buttonDelete__"+task.className);
+        buttonDelete.setAttribute("id", "buttonDelete__"+task.className);
         buttonDelete.appendChild(buttonText2);
         
         //Append all the created elements to unordered list
@@ -38,59 +37,45 @@ function AddTaskToDisplay() {
         descriptionOfTask.innerText = task.description;
         listElement.appendChild(titleOfTask);
         listElement.appendChild(descriptionOfTask);
-        listElement.appendChild(buttonDone);
         listElement.appendChild(buttonDelete);
         unorderedList.append(listElement);
 
+        //Clear Inputs title and description in Browser
         inputTitle.value = "";
-        inputaddDescription.value = "";
+        inputaddDescription.value = "";        
 
-        //Add Event Listener to ADD and DELETE BUTTON
-        let taskDeleteButton = document.querySelectorAll('button[class^="buttonDelete__"]');
-
+        //Add Event Listener to DELETE BUTTON
+        let taskDeleteButton = document.querySelectorAll('button[id^="buttonDelete__"]');
         taskDeleteButton.forEach(DeleteTaskFromDisplay);
-        function DeleteTaskFromDisplay(button) {
-            button.addEventListener('click', () => {
-            let deleteTask = button.className;
-            console.log(deleteTask);
-            let removeID = 0;
-            if (deleteTask.length === 20) {
-                removeID = deleteTask.slice(-1);
-            }
-            else if (deleteTask.length === 21) {
-                removeID = deleteTask.slice(-2);
-            }
-            let removeElementLen = document.getElementsByClassName(arrayOfTasks[removeID].className);
-            if (removeElementLen.length > 0) {
-                removeElementLen[0].remove();
-            }    
-            
-            });
-        }
     }       
 }
 
-//         for (const button of taskDoneButton) {
-//             button.addEventListener('click', () => {
-//                 let deleteTask = button.className;
-//                 console.log(deleteTask);
-//                 let removeID = 0;
-//                 if (deleteTask.length === 20) {
-//                     removeID = deleteTask.slice(-1);
-//                 }
-//                 else if (deleteTask.length === 21) {
-//                     removeID = deleteTask.slice(-2);
-//                 }
-//                 let removeElementLen = document.getElementsByClassName(arrayOfTasks[removeID].className);
-//                 if (removeElementLen.length > 0) {
-//                     removeElementLen[0].remove();
-//                 }
-//             })
-//         }
-//     }
-// }
+function DeleteTaskFromDisplay(button) {
+    button.addEventListener('click', () => {
+    let deleteTask = button.id;
+    let removeID = 0;
+    if (deleteTask.length === 20) {
+        removeID = deleteTask.slice(-1);
+    }
+    else if (deleteTask.length === 21) {
+        removeID = deleteTask.slice(-2);
+    }
+    let removeElementLen = document.getElementsByClassName(arrayOfTasks[removeID].className);
+    if (removeElementLen.length > 0) {
+        removeElementLen[0].remove();
+    }    
+    
+    });
+}
 
+unorderedList.addEventListener('click', AddOrRemoveTask, false);
 
-
+function AddOrRemoveTask(task) {
+    let clickedClass = task.target.className;
+    if (task.target !== task.currentTarget && task.target.className) {
+        let removeElement = document.getElementsByClassName(clickedClass);
+        removeElement[0].classList.toggle("removedElement")
+    }
+}
 
 
